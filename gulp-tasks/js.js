be05aee17e36a9d0330fec0adb03f3da2b4e168b
Ivan Hayes <git@ivanhayes.com>
2017-06-20 11:41:10 +0200

    Replace modified paths for resolving JS files

--- gulp-tasks/js.js
+++ gulp-tasks/js.js
@@ -22,9 +22,7 @@ const _compile = {
     return _browserify({entries: `${_paths.srcDir}${_paths.srcFile}`, debug: true})
       .plugin(_pathmodify, {
         mods: [
-          _pathmodify.mod.dir('layouts', process.cwd() + '/src/markup/templates/layouts'),
-          _pathmodify.mod.dir('views', process.cwd() + '/src/markup/templates/views'),
-          _pathmodify.mod.dir('components', process.cwd() + '/src/markup/partials/components')
+          _pathmodify.mod.dir('app', process.cwd() + '/src/js')
         ]
       })
       .transform('babelify', {presets: ['es2015']})
@@ -41,9 +39,7 @@ const _compile = {
     return _browserify({entries: `${_paths.srcDir}${_paths.srcFile}`, debug: false})
       .plugin(_pathmodify, {
         mods: [
-          _pathmodify.mod.dir('templates', process.cwd() + '/src/markup/templates'),
-          _pathmodify.mod.dir('components', process.cwd() + '/src/markup/partials/components'),
-          _pathmodify.mod.dir('views', process.cwd() + '/src/markup/partials/views')
+          _pathmodify.mod.dir('app', process.cwd() + '/src/js')
         ]
       })
       .transform('babelify', {presets: ['es2015']})
